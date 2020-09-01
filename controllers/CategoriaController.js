4ffe93f99f3a8d76b62bf1c9ea11dc91ffaff0e0
Guilherme <guilhermemendesousa@gmail.com>
2020-09-01 06:51:22 -0300

    - ALTERAÇÃO NO SELECT DE CATEGORIAS, ADICIONANDO TAMBÉM A DISPONIBILIDADE

--- controllers/CategoriaController.js
+++ controllers/CategoriaController.js
@@ -17,7 +17,7 @@ class CategoriaController {
     indexDisponiveis(req, res, next) {
         Categoria
             .find({ loja: req.query.loja, disponibilidade: true })
-            .select("_id produtos nome codigo loja")
+            .select("_id produtos nome codigo disponibilidade loja")
             .then((categorias) => res.send({ categorias }))
             .catch(next);
     }
@@ -26,7 +26,7 @@ class CategoriaController {
     show(req, res, next) {
         Categoria
             .findOne({ loja: req.query.loja, _id: req.params.id })
-            .select("_id produtos nome codigo loja")
+            .select("_id produtos nome codigo disponibilidade loja")
             .populate(["produtos"])
             .then(categoria => res.send({ categoria }))
             .catch(next);
