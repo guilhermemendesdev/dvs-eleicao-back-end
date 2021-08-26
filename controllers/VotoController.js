40c412a2ac7449115ecbaf8f527100fb4e5a5115
Guilherme Mendes <guilhermemendesousa@gmail.com>
2021-08-26 09:28:10 -0300

    feat: create and refactory function to 'voto'

--- controllers/VotoController.js
+++ controllers/VotoController.js
@@ -1,8 +1,12 @@
+'use strict'
+
 const mongoose = require('mongoose');
 const moment = require('moment')
 const Voto = mongoose.model('Voto');
 const Aluno = mongoose.model('Aluno');
+const Funcionario = mongoose.model('Funcionario');
 const Votacao = mongoose.model('Votacao');
+const { badRequest } = require('../helpers/http-helper')
 
 const EmailController = require('./EmailController');
 
@@ -10,24 +14,39 @@ class VotoController {
 
   async store(req, res, next) {
     const zona = req.payload.id;
-    const { candidato, idAluno, votoResp } = req.body
+    const { candidato, idVotante, tipoVoto } = req.body
     const votacao = await Votacao.findOne({ zona: zona })
     const hora = new Date
     const dataAtual = moment(hora).format('DD/MM/YYYY HH:mm:ss')
-    const aluno = await Aluno.findOne({ _id: idAluno })
-    if (votoResp === false) aluno.aluno_votou = true
-    if (votoResp === true) aluno.resp_votou = true
-    const voto = new Voto({
-      zona: zona,
-      data_hora_voto: dataAtual,
-      candidato: candidato
-    })
-    votacao.voto.push(voto._id)
+
     try {
+      if (tipoVoto === 'aluno' || tipoVoto === 'resp') {
+        const aluno = await Aluno.findOne({ _id: idVotante })
+        if (tipoVoto === 'aluno' && aluno.aluno_votou === true) throw badRequest('Aluno já votou!')
+        if (tipoVoto === 'resp' && aluno.resp_votou === true) throw badRequest('Responsável já votou!')
+        if (tipoVoto === 'aluno') aluno.aluno_votou = true
+        if (tipoVoto === 'resp') aluno.resp_votou = true
+        await aluno.save();
+      }
+
+      if (tipoVoto === 'func') {
+        const funcionario = await Funcionario.findOne({ _id: idVotante })
+        if (funcionario.votou === true) throw badRequest('Funcionário já votou!')
+        funcionario.votou = true
+        await funcionario.save();
+      }
+
+      const voto = new Voto({
+        zona: zona,
+        data_hora_voto: dataAtual,
+        tipo_voto: tipoVoto,
+        candidato: candidato
+      })
+      votacao.voto.push(voto._id)
+
       await voto.save();
       await votacao.save();
-      await aluno.save();
-      return res.send({ voto });
+      return res.send({ voto })
     } catch (e) {
       next(e)
     }
