de522806ccd529acda7cce903092df477b2ea2df
Guilherme Mendes <guilhermemendesousa@gmail.com>
2021-08-31 10:03:50 -0300

    refactor: remove link to router of chapa

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -5,7 +5,6 @@ router.use('/votacao', require('./votacao'));
 router.use('/zona', require('./zona'));
 router.use('/aluno', require('./aluno'));
 router.use('/funcionario', require('./funcionario'));
-router.use('/chapa', require('./chapa'));
 router.use('/usuarios', require('./usuarios'));
 router.use('/horarioBrasilia', require('./horarioBrasilia'));
 router.use('/voto', require('./voto'));
