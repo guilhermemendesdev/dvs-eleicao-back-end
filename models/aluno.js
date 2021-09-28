76fcfcb3b03a18bff80fbf5a4fc6b52cb90b280a
Guilherme <guilhermemendesousa@gmail.com>
2021-09-28 10:49:59 -0300

    refactor: change type idescola

--- models/aluno.js
+++ models/aluno.js
@@ -5,12 +5,10 @@ const mongoosePaginate = require("mongoose-paginate");
 
 const AlunoSchema = new mongoose.Schema({
   idescola: {
-    type: Schema.Types.ObjectId,
-    ref: 'Zona'
+    type: String,
   },
   inep: {
     type: String,
-
   },
   nome: {
     type: String,
