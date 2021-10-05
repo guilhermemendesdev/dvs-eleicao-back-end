9e4e0d840ccac3a5c1e0a98ee9f2270b88cda425
Guilherme <guilhermemendesousa@gmail.com>
2021-10-05 13:03:09 -0300

    refactor: change route funcionario

--- routes/api/v1/funcionario.js
+++ routes/api/v1/funcionario.js
@@ -11,9 +11,9 @@ const auth = require("../../auth");
 const funcionarioController = new FuncionarioController();
 
 // ADM
-router.get('/adm', auth.required, ZonaValidation.adm, funcionarioController.indexAdm);
-router.get('/adm/:id', auth.required, ZonaValidation.adm, funcionarioController.showAdm);
-router.get("/adm/search/:search", auth.required, ZonaValidation.adm, funcionarioController.searchAlunos);
+router.get('/', auth.required, ZonaValidation.adm, funcionarioController.indexAdm);
+router.get('/:id', auth.required, ZonaValidation.adm, funcionarioController.showAdm);
+router.get("/search/:search", auth.required, ZonaValidation.adm, funcionarioController.searchAlunos);
 // router.post("/", auth.required, AdmValidation.adm, validate(ZonaValidation.store), funcionarioController.store); //testado
 // router.put("/:id", auth.required, ZonaValidation.adm, validate(AlunoValidation.update), funcionarioController.update); //testado
 // router.delete("/:id", auth.required, AdmValidation.adm, validate(ZonaValidation.remove), funcionarioController.remove); //testado
